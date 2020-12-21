cc2f445f6048221732330fe3c6247d6aaafb8d07
Lucas Ballyn <ballyn.lucas@gmail.com>
2020-12-21 23:03:16 +0100

    Init quizmaster ui

--- client/src/Pages/PlayGame.vue
+++ client/src/Pages/PlayGame.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <div class="w-100 flex flex-row">
-      <div>Hallo</div>
-      <div>Helo</div>
-      <div>Lets go</div>
+    <div class="flex flex-row space-x-2">
+      <div class="w-1/3 h-56 bg-gray-400">1</div>
+      <div class="w-1/3 h-56 bg-gray-400">2</div>
+      <div class="w-1/3 h-56 bg-gray-400">3</div>
     </div>
-    <div></div>
+    <div class="flex flex-row space-x-4 flex-grow">
+      <div class="h-100 w-1/2 bg-gray-400">
+      </div>
+      <div class="w-1/2 flex flex-col">
+        <div class="flex flex-row">
+          <button class="w-1/2">Start</button>
+          <button class="w-1/2">Stop</button>
+        </div>
+        <div class="flex flex-row flex-wrap">
+          <button class="w-1/4">+10</button>
+          <button class="w-1/4">+15</button>
+          <button class="w-1/4">+20</button>
+          <button class="w-1/4">+30</button>
+          <button class="w-1/4">+40</button>
+          <button class="w-1/4">+50</button>
+        </div>
+        <div class="flex">
+          <button>-20</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

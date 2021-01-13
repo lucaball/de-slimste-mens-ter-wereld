0eb80d48b9546b222771c43f758549e6c755ba2b
Lucas Ballyn <ballyn.lucas@gmail.com>
2021-01-13 22:05:03 +0100

    Listen to different port

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     app.useStaticAssets(join(__dirname, '..', 'client/public/build'));
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 8080);
 
     Logger.verbose("App running on " + await app.getUrl());
 }

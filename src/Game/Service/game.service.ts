8b642961afdb690dcde92840cc537f101587fd4c
Lucas Ballyn <ballyn.lucas@gmail.com>
2021-01-13 22:19:04 +0100

    Fix joincode ref

--- src/Game/Service/game.service.ts
+++ src/Game/Service/game.service.ts
@@ -28,7 +28,7 @@ export class GameService {
             .leftJoinAndSelect("rounds.questions", "questions")
             .leftJoinAndSelect("questions.answers", "answers")
             .leftJoinAndSelect("game.gamePlayers", "gamePlayers")
-            .where('joinCode = :joinCode', { joinCode: joinCode })
+            .where('game.joinCode = :joinCode', { joinCode: joinCode })
             .orderBy('rounds.position', 'ASC')
             .getOne();
     }
